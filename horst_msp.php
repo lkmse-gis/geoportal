724df117d3742c5a1f5dd17ada197fff82d11b15
Andreas Thurm <andreas.thurm@lk-seenplatte.de>
2020-06-17 16:22:11 +0200

    Horststandorte

--- horst_msp.php
+++ horst_msp.php
@@ -1,12 +1,19 @@
 <?php
 include ("includes/connect_geobasis.php");
-include ("includes/connect.php");
+include ("includes/connect_i_procedure_mse.php");
 include ("includes/portal_functions.php");
+require_once ("classes/karte.class.php");
+require_once ("classes/legende_geo.class.php");
+$layer_legende="Horststandorte";
+$layer_legende_2="Kreisgrenze_msp";
+$layer_legende_3="msp_outline_gem";
+$layer="Horststandorte";
 
 //globale Varibalen
 $titel="Horststandorte";
-$titel2="Horststandort";
-$datei="horst_msp.php";
+$label_auswahl="Horststandort";
+$beschriftung_karte="Horststandort";
+$datei=$_SERVER["PHP_SELF"];
 $tabelle="sg_spa_punkt";
 $schema="environment";
 $kuerzel="horst";
@@ -16,7 +23,7 @@ $gemeinde_id=$_GET["gemeinde"];
 $horst_id=$_GET["$kuerzel"];
 $themen_id=$horst_id;
 
-$log=write_log($db_link,$layerid);
+$log=write_i_log($db_link,$layerid);
 
 if ($themen_id < 1 AND $gemeinde_id < 1)
     { 
@@ -24,7 +31,7 @@ if ($themen_id < 1 AND $gemeinde_id < 1)
 		$query="SELECT COUNT(*) AS anzahl FROM $schema.$tabelle";	  
 		$result = $dbqueryp($connectp,$query);
 		$r = $fetcharrayp($result);
-		$count = $r[anzahl];
+		$count = $r["anzahl"];
 	
 	?>
 		<?php
@@ -39,14 +46,9 @@ if ($themen_id < 1 AND $gemeinde_id < 1)
 		<link rel="stylesheet" type="text/css" href="styles.css" />
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 		<? include ("ajax.php"); ?>
-		<? include ("includes/zeit.php"); ?>
-		<? include ("includes/meta_popup.php"); ?>
-		<? include ("includes/block_1_css_map.php"); ?>			
-		<script src=<? echo $openlayers_url; ?> type="text/javascript" language="Javascript"></script>
-			<link rel="stylesheet" href=<? echo $olstyles_url; ?> type="text/css" />
-		<script type="text/javascript" language="Javascript">
-			<? include ("includes/block_1.php"); ?>
-		</script>
+		<? include ("includes/zeit.php"); 
+        $spa_karte= new karte;
+        echo $spa_karte->zeigeKarteBox($box_mse_gesamt,'740','450','orka','1','0','0','0','0',$beschriftung_karte,$layer);?>
 		<script type="text/javascript" language="JavaScript1.2" src="um_menu.js"></script>
 		
 		</head>
@@ -60,7 +62,26 @@ if ($themen_id < 1 AND $gemeinde_id < 1)
 			<div id="wrapper">
 				<div id="content">
 					<table width="100%" border=0 cellpadding="0" align="center" cellspacing="0">
-						<? include ("includes/count_map.php"); ?>
+									<!-- linke Spalte mit Auswahlfeld, Kartenlegende usw. -->
+									
+									<td align="center" valign="top" height=30 colspan=2><br>
+										<h3><? echo $titel; ?>*</h3>
+										Zu diesem Thema befinden sich<br>
+										<b><? echo $count; ?></b> Datens&auml;tze in der Datenbank.
+									</td>
+									
+									<!-- schmale Zwischenspalte für den Abstand zur Karte -->
+									
+									<td width=30 rowspan=8></td>
+									
+									<!-- Spalte für die Karte, der Map-Container wird in der Klasse Karte erzeugt -->
+									
+									<td border=0 valign=top align=center rowspan=7 colspan=3>
+										<br>
+										<div style="margin:1px" id="map"></div>
+									</td>
+								</tr>
+
 						<tr>
 							<td align="center" height=50 colspan=2>
 								Gemeinde ausw&auml;hlen:
@@ -84,35 +105,28 @@ if ($themen_id < 1 AND $gemeinde_id < 1)
 								</form>
 							</td>
 						</tr>							
-						<tr>
-					             <td valign=bottom align="center" colspan=2>
-					    *) <a href="metadaten/metadaten.php?Layer_ID=<? echo $layerid;?>" target="_blank" onclick="return meta_popup(this.href)">Info zum Thema <? echo $titel;?></a>
-						         </td>
-					            </tr>
-					            <tr><td align="center" colspan=2>letzte Aktualisierung: <b><i><? echo get_aktualitaet($dbname,$layerid); ?></td>
-								</tr>
-                                <tr>
-									<td valign=bottom align=center>
-										<a href="../both/wissenswertes/kartehilfe.php" target="_blank" onclick="return hilfe_popup(this.href)">Wie nutze ich die webbasierte Karte?<br>(OpenLayers)</a>																
-									</td>
-								</tr>
-								<tr>
-									<td valign=bottom align=right>
-										<!-- Tabelle für Legende -->
-										<table border="1" rules="none" valign=bottom align=right>					
-											<tr>
-												<td colspan=4 align=center height=25><i>Kartenlegende:</i></td>
-											</tr>
-											<tr>
-												<td align=right><small><? echo $titel2;?>: </td>
-												<td align=right><img src="images/spa_pkt.gif" width=20></td>
-												<td align=right><small>Kreisgrenze: </td>
-												<td align=right><img src="images/gemeindegrenze_2.png" width=30></td>
-											</tr>																					
-										</table> <!-- Ende der Tabelle für die Legende -->
-									</td>
-					           </tr>						
-						<? include ("includes/block_1_uk.php"); ?>						
+			<!-- Einbindung eines Snippets mit der Verknüpfung zu den Metadaten (3 Zeilen)-->
+			
+						<? include ("includes/meta_i_aktualitaet.php"); ?>
+						
+			<!-- Zeile für die Legende -->
+								
+								<tr>									
+ 			                       <td valign=bottom align=left >
+							       <table class="table_legende" >
+								    <B>Kartenlegende :</B>
+								    <?php
+								     $legende_geo= new legende_geo;
+								     echo $legende_geo->zeigeLegende($layer_legende,$layer_legende_2,'','','');
+								     ?>
+							       </table> 
+						          </td>
+    		                   	</tr>
+																			
+
+			 <!-- Einbindung des Snippets für die Zeile unter der Karte -->
+							   
+			    <? include ("includes/block_1_1_uk.php"); ?>				
 					</table>
 				</div>
 			</div>
@@ -148,32 +162,13 @@ if ($gemeinde_id > 0)
 	  $amtname=$r[0];
 	  $amt=$r[1];
 	  
-	  $query="SELECT box(st_transform(a.the_geom,25833)) as etrsbox, st_astext(st_transform(st_centroid(a.the_geom),25833)) as etrscenter, box(a.the_geom) as box, area(a.the_geom) as area, st_astext(st_centroid(a.the_geom)) as center, a.gemeinde as name FROM gemeinden as a WHERE a.gem_schl='$gemeinde_id'";
+	  $query="SELECT box(st_transform(a.the_geom,25833)) as etrsbox,  a.gemeinde as name FROM gemeinden as a WHERE a.gem_schl='$gemeinde_id'";
 	  
 	  $result = $dbqueryp($connectp,$query);
 	  $r = $fetcharrayp($result);
-	  $gemeindename = $r[name];
-	  $zentrum = $r[etrscenter];
-	  $zentrum2 = trim($zentrum,"POINT(");
-	  $zentrum3 = trim($zentrum2,")");
-	  $zentrum4 = explode(" ",$zentrum3);
-	  $rcenter = $zentrum4[0];
-	  $hcenter = $zentrum4[1];
-	  $boxstring = $r[etrsbox];
-	  $klammern=array("(",")");
-	  $boxstring = str_replace($klammern,"",$boxstring);
-	  $koordinaten = explode(",",$boxstring);
-	  $rechts_range = $koordinaten[0]-$koordinaten[2];
-	  $rechts = $koordinaten[2]+($rechts_range/2);
-	  $hoch_range = $koordinaten[1]-$koordinaten[3];
-	  $hoch = $koordinaten[3]+($hoch_range/2);
-	  $range = $hoch_range;
-	  if ($rechts_range > $hoch_range) $range=$rechts_range;
-    
-
-        $lon=$rechts;
-		$lat=$hoch;
-		?>
+	  $gemeindename = $r["name"];
+	  $etrsbox = $r["etrsbox"];
+	  ?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
@@ -184,16 +179,8 @@ if ($gemeinde_id > 0)
 		<? include ("ajax.php"); ?>
 		<? include ("includes/zeit.php"); ?>
 		<? include ("includes/meta_popup.php"); ?>
-		<? include ("includes/bilder_popup.php"); ?>
-		<? include ("includes/block_2_css_map.php"); ?>
-		<script src=<? echo $openlayers_url; ?> type="text/javascript" language="Javascript"></script>
-			<link rel="stylesheet" href=<? echo $olstyles_url; ?> type="text/css" />		
-		<script type="text/javascript" language="Javascript">
-			<? include ("includes/block_2.php"); ?>
-		</script>
-		<style type="text/css">
-			td.rand {border: solid #000000 1px;}
-		</style>
+		<?php $spa_karte= new karte;
+		echo $spa_karte->zeigeKarteBox($etrsbox,'680','450','orka','1','0','1','0','0',$beschriftung_karte,$layer); ?>			 
 		<script type="text/javascript" language="JavaScript1.2" src="um_menu.js"></script>
 		
 		</head>
@@ -235,7 +222,7 @@ if ($gemeinde_id > 0)
 
 														while($r = $fetcharrayp($result))
 															{
-																echo "<option";if ($gemeinde_id == $r[gem_schl]) echo " selected"; echo " value=\"$r[gem_schl]\">$r[gemeinde]</option>\n";
+																echo "<option";if ($gemeinde_id == $r["gem_schl"]) echo " selected"; echo ' value="',$r["gem_schl"],'">',$r["gemeinde"],'</option>\n';
 															}
 													?>
 												</select>
@@ -250,21 +237,19 @@ if ($gemeinde_id > 0)
 										
 									</tr>
 									<tr><td  height=10></td></tr>
-									<tr>										
-										<td valign=bottom align=right>
-										<table border="1" rules="none" valign=bottom align=right>					
-											<tr>
-												<td colspan=4 align=center height=25><i>Kartenlegende:</i></td>
-											</tr>
-											<tr>
-												<td align=right><small><? echo $titel2;?>: </td>
-												<td align=right><img src="images/spa_pkt.gif" width=20></td>
-												<td align=right><small>Gemeindegrenze: </td>
-												<td align=right><img src="images/gemeindegrenze_2.png" width=30></td>
-											</tr>																					
-										</table>
-										</td>
-									</tr>									
+								<!-- Zeile für die Legende -->
+								
+								   <tr>									
+ 			                       <td valign=bottom align=left >
+							       <table class="table_legende" >
+								    <B>Kartenlegende :</B>
+								    <?php
+								     $legende_geo= new legende_geo;
+								     echo $legende_geo->zeigeLegende($layer_legende,$layer_legende_2,$layer_legende_3,'','');
+								     ?>
+							       </table> 
+						          </td>
+    		                   	</tr>
 									<? include ("includes/block_2_uk_gem.php"); ?>	
 								</table> <!-- Ende innere Tablle oberer Block -->
 							</td>
@@ -286,9 +271,9 @@ if ($gemeinde_id > 0)
 													{ 
 														echo "<tr bgcolor=",get_farbe($v),">";	
 														echo "														
-														<td align='center' height='30'><a href=\"$datei?$kuerzel=",$horst[$v][gid],"\">",$horst[$v][horst_nr],"</a></td>",														
-														"<td align='center'>",$horst[$v][ort],"</td>",
-														"<td align='center'>",$horst[$v][art],"</td></tr>";
+														<td align='center' height='30'><a href=\"$datei?$kuerzel=",$horst[$v]["gid"],"\">",$horst[$v]["horst_nr"],"</a></td>",														
+														"<td align='center'>",$horst[$v]["ort"],"</td>",
+														"<td align='center'>",$horst[$v]["art"],"</td></tr>";
 													}
 												?>																																				
 											</table>
@@ -321,36 +306,19 @@ if ($gemeinde_id > 0)
 	  $query="SELECT a.amt, a.amt_id, a.gemeinde, a.gem_schl as gemeindeid, b.gid FROM gemeinden as a, $schema.$tabelle as b WHERE ST_WITHIN(b.geom, a.geom_25833) AND b.gid='$themen_id'";
 	  $result = $dbqueryp($connectp,$query);
 	  $r = $fetcharrayp($result);
-	  $amtname=$r[amt];
-	  $amt=$r[amt_id];
-	  $gem_id=$r[gemeindeid];
-	  $gemeindename=$r[gemeinde];
+	  $amtname=$r["amt"];
+	  $amt=$r["amt_id"];
+	  $gem_id=$r["gemeindeid"];
+	  $gemeindename=$r["gemeinde"];
 	  
-	  $query="SELECT astext(st_transform(geom,2398)) as koordinaten, astext(geom) as utm, st_astext(st_centroid(st_transform(geom, 31468))) as rd83, astext(st_transform(geom, 4326)) as geo, gid, ort, horst_nr, art FROM $schema.$tabelle WHERE gid='$themen_id'";
+	  $query="SELECT st_transform(geom,25833) as geom_25833, box(st_transform(geom,25833)) as etrsbox, gid, ort, horst_nr, art FROM $schema.$tabelle WHERE gid='$themen_id'";
 	  
 	  $result = $dbqueryp($connectp,$query);
 	  $r = $fetcharrayp($result);
-	  $koord = $r[koordinaten];
-	  $koord2 = trim($koord,"POINT(");
-	  $koord3 = trim($koord2,")");
-	  $koord4 = explode(" ",$koord3);
-	  $rd83 = $r[rd83];
-	  $utm = $r[utm];
-	  $geo=$r[geo];
-	  $utm2 = trim($utm,"POINT(");
-	  $utm3 = trim($utm2,")");
-	  $utm4 = explode(" ",$utm3);
-	  $utm5 = explode(".",$utm4[0]);
-	  $utm_lon = $utm5[0];
-	  $utm6 = explode(".",$utm4[1]);
-	  $utm_lat = $utm6[0];
-	  $lon = $koord4[0];
-	  $lon1 = explode(".",$koord4[0]);
-	  $lon2 = $lon1[0];
-	  $lat = $koord4[1];
-	  $lat1 = explode(".",$koord4[1]);
-	  $lat2 = $lat1[0];
-		?>
+	  $geom_25833=$r["geom_25833"];
+	  $etrsbox=$r["etrsbox"];
+	  
+	  ?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
@@ -361,16 +329,8 @@ if ($gemeinde_id > 0)
 		<? include ("ajax.php"); ?>
 		<? include ("includes/zeit.php"); ?>
 		<? include ("includes/meta_popup.php"); ?>
-		<? include ("includes/bilder_popup.php"); ?>
-		<? include ("includes/block_3_css_map.php"); ?>
-		<script src=<? echo $openlayers_url; ?> type="text/javascript" language="Javascript"></script>
-			<link rel="stylesheet" href=<? echo $olstyles_url; ?> type="text/css" />
-		<script type="text/javascript" language="Javascript">
-			<? include ("includes/block_3.php"); ?>
-		</script>
-		<style type="text/css">
-			td.rand {border: solid #000000 2px;}
-		</style> 
+		<?php $spa_karte= new karte;
+		echo $spa_karte->zeigeKarteBox($etrsbox,'680','450','orka','1','0','1','0','0',$beschriftung_karte,$layer); ?>			 
 		<script type="text/javascript" language="JavaScript1.2" src="um_menu.js"></script>
 		
 		</head>
@@ -389,7 +349,7 @@ if ($gemeinde_id > 0)
 								<table border=0>
 									<tr>
 										<td height="40" align="center" valign=center width=250 colspan="2" bgcolor=<? echo $header_farbe; ?>>
-											<? echo $font_farbe ;?>Horststandort <? echo $r[horst_nr]; ?><? echo $font_farbe_end ;?>
+											<? echo $font_farbe ;?>Horststandort <? echo $r["horst_nr"]; ?><? echo $font_farbe_end ;?>
 										</td>
 										<td width=10 rowspan="7"></td>
 										<td border=0 valign=top align=left rowspan="6" colspan=3>
@@ -412,7 +372,7 @@ if ($gemeinde_id > 0)
 
 														while($e = $fetcharrayp($result))
 															{
-																echo "<option";if ($gem_id == $e[gem_schl]) echo " selected"; echo " value=\"$e[gem_schl]\">$e[gemeinde]</option>\n";
+																echo "<option";if ($gem_id == $e["gem_schl"]) echo " selected"; echo ' value="',$e["gem_schl"],'">',$e["gemeinde"],'</option>\n';
 															}
 													?>
 												</select>
@@ -429,40 +389,42 @@ if ($gemeinde_id > 0)
 											<a href="<? echo $datei;?>?gemeinde=<? echo $gem_id; ?>"><? echo $font_farbe ;?>alle <? echo $titel;?><br><? echo $gemeindename ?><? echo $font_farbe_end ;?></a>
 										</td>										
 									</tr>
-									<tr>										
-										<td valign=bottom align=right>
-											<table border="1" rules="none" valign=bottom align=right>					
-												<tr>
-													<td colspan=4 align=center height=25><i>Kartenlegende:</i></td>
-												</tr>
-												<tr>
-													<td align=right><small><? echo $titel2;?>: </td>
-													<td align=right><img src="images/spa_pkt.gif" width=20></td>													
-												</tr>																					
-											</table>
-										</td>
-									</tr>
-									<? include ("includes/block_3_uk.php"); ?>	
+			<!-- Zeile für die Legende -->
+								
+								<tr>									
+ 			                       <td valign=bottom align=left >
+							       <table class="table_legende" >
+								    <B>Kartenlegende :</B>
+								    <?php
+								     $legende_geo= new legende_geo;
+								     echo $legende_geo->zeigeLegende($layer_legende,$layer_legende_2,'','','');
+								     ?>
+							       </table> 
+						          </td>
+    		                   	</tr>
+			 <!-- Einbindung des Snippets für die Zeile unter der Karte -->
+							   
+			    <? include ("includes/block_1_1_uk.php"); ?>				
                                  </table>								 
 								<table width="100%" border="0" cellpadding="0" align="center" cellspacing="0">
 								<tr>
 								<td valign=top>											
 									<table border=0 valign=top>
 										<tr height="35">
-											<td colspan=3 width="620" bgcolor=<? echo $header_farbe ;?>>&nbsp;&nbsp;<? echo $font_farbe ;?><font size="+1">Horststandort <? echo $r[horst_nr] ;?><? echo $font_farbe_end ;?></td>													
+											<td colspan=3 width="620" bgcolor=<? echo $header_farbe ;?>>&nbsp;&nbsp;<? echo $font_farbe ;?><font size="+1">Horststandort <? echo $r["horst_nr"] ;?><? echo $font_farbe_end ;?></td>													
 										</tr>
 										<tr height="30">
 											<td bgcolor=<? echo $element_farbe ?>>Ort:</td>
-											<td bgcolor=<? echo $element_farbe ?>><b><? echo $r[ort] ;?></b></td>									
+											<td bgcolor=<? echo $element_farbe ?>><b><? echo $r["ort"] ;?></b></td>									
 										</tr>
 										<tr height="30">
 											<td>Art:</td>
-											<td><b><? echo $r[art] ;?></b></td>																									
+											<td><b><? echo $r["art"] ;?></b></td>																									
 										</tr>																										
 									</table>
 								</td>									
 							<td valign=top align=center width="350">
-							<? include("includes/geo_point.php"); ?>	
+							<? echo geo_punkt($geom_25833,$connectp,$dbqueryp,$fetcharrayp) ?>	
 							</td>
 						</tr>
 						</table>
