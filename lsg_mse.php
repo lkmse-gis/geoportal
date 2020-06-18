d5c685d3d0d41ce393107fb097cbb92406f51f04
Andreas Thurm <andreas.thurm@lk-seenplatte.de>
2020-06-18 10:14:41 +0200

    LSG

--- lsg_mse.php
+++ lsg_mse.php
@@ -1,14 +1,17 @@
 <?php
 include ("includes/connect_geobasis.php");
-include ("includes/connect_i_mse.php");
+include ("includes/connect_i_procedure_mse.php");
 include ("includes/portal_functions.php");
 require_once ("classes/karte.class.php");
-require_once ("classes/legenden.class.php");
+require_once ("classes/legende_geo.class.php");
+$layer_legende="lsg_2016";
+$layer_legende_2="Kreisgrenze_msp";
+$layer="lsg_2016";
+$label_auswahl="LSG";
 
 
 //globale Varibalen
 $titel="Landschaftsschutzgebiete";
-$layer_name="lsg_2016";
 $beschriftung_karte="LSG";
 $tabelle="sg_lsg_2016";
 $schema="environment";
@@ -24,7 +27,7 @@ if ($lsg_id < 1)
 		$query="SELECT COUNT(*) AS anzahl FROM $schema.$tabelle";	  
 		$result = $dbqueryp($connectp,$query);
 		$r = $fetcharrayp($result);
-		$count = $r[anzahl];
+		$count = $r["anzahl"];
 	
 		$lon=368607;
 		$lat=5937811;
@@ -41,7 +44,7 @@ if ($lsg_id < 1)
 		<? include ("includes/meta_popup.php"); ?>
 		<?
              $lsg_karte= new karte;
-             echo $lsg_karte->zeigeKarteBox($box_mse_gesamt,'690','490','orka','1','0','0','0','0',$beschriftung_karte,$layer_name);			 
+             echo $lsg_karte->zeigeKarteBox($box_mse_gesamt,'690','490','orka','1','0','0','0','0',$beschriftung_karte,$layer);			 
             ?>
 		<script type="text/javascript" language="JavaScript1.2" src="um_menu.js"></script>
 		
@@ -93,42 +96,38 @@ if ($lsg_id < 1)
 										</form>
 									</td>									
 								</tr>
-                                <tr>
-					             <td valign=bottom align=center colspan=2>
-					    *) <a href="<? echo $metadatenpfad.$layerid;?>" target="_blank" onclick="return meta_popup(this.href)">Info zum Thema<br><? echo $titel; ?></a>
-						         </td>
-					            </tr>
-					            <tr><td align=center colspan=2>letzte Aktualisierung: <b><i><? echo get_i_aktualitaet($db_link,$layerid); ?></td>
-								</tr>
-								<tr>
-									<td valign=bottom align=center>
-										<a href="wissenswertes/kartehilfe.php" target="_blank" onclick="return hilfe_popup(this.href)">Wie nutze ich die webbasierte Karte?<br>(OpenLayers)</a>																
-									</td>
-								</tr>
-                                <?php
-											  $lsglegende=new legende;
-											  echo $lsglegende->lsg();
-								?>							
-								<tr>
-										<td colspan=2 height=35></td>                                       										
-										<td><small>
-											 <? echo $cr; ?>| &nbsp;<a href="wissenswertes/kartehilfe.php" target="_blank" onclick="return hilfe_popup(this.href)">Hilfe zur Kartennutzung</a>
-										</td>	
-										<td>   
-											<a href="<? echo $metadatenpfad.$layerid;?>" target="_blank" onclick="return meta_popup(this.href)"><img src="images/info_button.gif" title="Metadaten" border=0></a>
-										</td>
-										<td align=right>
-											<a href="<? echo $_SERVER["PHP_SELF"];?>"><img src="images/reload.png" title="Kartenausschnitt neu laden"></a>
-										</td>
-									</tr>
+								<!-- es folgt die Einbindung eines Snippets mit der Verknüpfung zu den Metadaten -->
+								
+                                <? include ("includes/meta_i_aktualitaet.php"); ?> 
+								
+								<!-- Zeile für die Legende -->
+								
+								<tr>									
+ 			                       <td valign=bottom align=left >
+							       <table class="table_legende" >
+								    <B>Kartenlegende :</B>
+								    <?php
+								     $legende_geo= new legende_geo;
+								     echo $legende_geo->zeigeLegende($layer_legende,$layer_legende_2,'','','');
+								     ?>
+							       </table> 
+						          </td>
+    		                   	</tr>
+
+							   <!-- Einbindung des Snippets für die Zeile unter der Karte -->
+							   
+					           <? include ("includes/block_1_1_uk.php"); ?>	
 							</table>
 						</div>
 					</div>
-					<?php 
-			        echo div_navigation(); 
-			        echo div_extra(); 
-			        echo div_footer(); 
-			        ?>
+			        <div id="navigation">
+				    <? include ("includes/navigation.php"); ?>
+			        </div>
+			        <div id="extra">
+				    <? include ("includes/news.php"); ?>
+			        </div>
+			        <div id="footer">			
+		            </div>
 				</div>
 			</body>
 		</html>
@@ -161,8 +160,8 @@ if ($lsg_id > 0)
 	  $result = $dbqueryp($connectp,$query);
 	  $r = $fetcharrayp($result);	 
 	  
-	  $boxstring = $r[box];
-	  $geometrie = $r[geom];
+	  $boxstring = $r["box"];
+	  $geometrie = $r["geom"];
 	  ?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml">
@@ -176,7 +175,7 @@ if ($lsg_id > 0)
 		<? include ("includes/meta_popup.php"); ?>
 		<?
              $lsg_karte= new karte;
-             echo $lsg_karte->zeigeKarteBox($boxstring,'700','320','orka','1','0','0','0','0',$beschriftung_karte,$layer_name);			 
+             echo $lsg_karte->zeigeKarteBox($boxstring,'700','320','orka','1','0','0','0','0',$beschriftung_karte,$layer);			 
         ?> 
 		<script type="text/javascript" language="JavaScript1.2" src="um_menu.js"></script>
 		<script type="text/javascript">
@@ -203,7 +202,7 @@ if ($lsg_id > 0)
 								<table border=0>
 									<tr>
 										<td height="40" align="center" valign=center width=250 colspan="2" bgcolor=<? echo $header_farbe; ?>>
-											<? echo $font_farbe ;?><? echo $r[name]; ?><? echo $font_farbe_End ;?>
+											<? echo $font_farbe ;?><? echo $r["name"]; ?><? echo $font_farbe_End ;?>
 										</td>
 										<td width=30 rowspan=7></td>
 										<td border=0 align=center rowspan="6" colspan=3>
@@ -212,11 +211,11 @@ if ($lsg_id > 0)
 									</tr>
 									<tr>
 										<td align="center" height="35" valign=center colspan="2" bgcolor=<? echo $element_farbe; ?>>
-											<b>GIS-Code: <? echo $r[gis_code]; ?></b><br>											
+											<b>GIS-Code: <? echo $r["gis_code"]; ?></b><br>											
 										</td>
 									</tr>
 									<tr>
-										<td align="center" height="25" colspan="2"><? echo $titel2;?>:</td>										
+										<td align="center" height="25" colspan="2"><? echo $label_auswahl;?>:</td>										
 									</tr>
 									<tr>
 										<td align="center" height="25" colspan="2">
@@ -227,7 +226,7 @@ if ($lsg_id > 0)
 														$result = $dbqueryp($connectp,$query);														
 														while($e = $fetcharrayp($result))
 														{
-														 echo "<option";if ($lsg_id == $e[gid]) echo " selected"; echo " value=\"$e[gid]\" title=\"$e[name]\">$e[name]</option>\n";
+														 echo "<option";if ($lsg_id == $e["gid"]) echo " selected"; echo ' value="',$e["gid"],'" title="',$e["name"],'">',$e["name"],'</option>\n';
 														}
 													?>
 												</select>
@@ -239,23 +238,22 @@ if ($lsg_id > 0)
 											<a href="<? echo $_SERVER["PHP_SELF"];?>"><? echo $font_farbe ;?>alle <? echo $titel;?><? echo $font_farbe_end ;?></a>
 										</td>										
 									</tr>									
-									<?php
-											  $lsglegende=new legende;
-											  echo $lsglegende->lsg();
-								    ?>	
-									<tr>
-										<td colspan=2></td>                                       										
-										<td><small>
-											 <? echo $cr; ?>| &nbsp;<a href="wissenswertes/kartehilfe.php" target="_blank" onclick="return hilfe_popup(this.href)">Hilfe zur Kartennutzung</a>
-										</td>	
-										<td>
-											<a href="metadaten/meta_info_small.php?id=<? echo $layerid;?>" target="_blank" onclick="return meta_popup(this.href)"><img src="images/info_button.gif" title="Metadaten" border=0></a>
-										</td>
-										<td align=right>
-											<a href="<? echo $_SERVER["PHP_SELF"];?>?<? echo $kuerzel;?>=<? echo $lsg_id; ?>"><img src="images/reload.png" title="Kartenausschnitt neu laden"></a>
-										</td>
-									</tr>
-								</table>
+								<!-- Zeile für die Legende -->
+								
+								   <tr>									
+ 			                       <td valign=bottom align=left >
+							       <table class="table_legende" >
+								    <B>Kartenlegende :</B>
+								    <?php
+								     $legende_geo= new legende_geo;
+								     echo $legende_geo->zeigeLegende($layer_legende,$layer_legende_2,'','','');
+								     ?>
+							       </table> 
+						          </td>
+    		                   	</tr>
+							   <!-- Einbindung des Snippets für die Zeile unter der Karte -->
+							   
+					           <? include ("includes/block_1_1_uk.php"); ?>				</table>
 							</td>
 						</tr>
 					</table>
@@ -264,18 +262,18 @@ if ($lsg_id > 0)
 							<td valign=top>											
 								<table border=0 valign=top>
 									<tr height="35">
-										<td colspan=2  width="620" bgcolor=<? echo $header_farbe ;?>>&nbsp;&nbsp;<? echo $font_farbe ;?><font size="+1"><? echo $r[name] ;?><? echo $font_farbe_end ;?></td>													
+										<td colspan=2  width="620" bgcolor=<? echo $header_farbe ;?>>&nbsp;&nbsp;<? echo $font_farbe ;?><font size="+1"><? echo $r["name"] ;?><? echo $font_farbe_end ;?></td>													
 									</tr>
 									<tr height="30">
 										<td bgcolor=<? echo $element_farbe ?> width="30%">Ausweisung:</td>
-										<td bgcolor=<? echo $element_farbe ?>><b><? echo $r[ausweis_mv] ;?></b></td>													
+										<td bgcolor=<? echo $element_farbe ?>><b><? echo $r["ausweis_mv"] ;?></b></td>													
 									</tr>
 									<tr height="30">
 										<td>Fl&auml;che in ha:</td>
-										<td><b><? echo $r[area_ha] ;?></b></td>													
+										<td><b><? echo $r["area_ha"] ;?></b></td>													
 									</tr>																		
 									<tr>
-										<td bgcolor=<? echo $element_farbe ?> valign=top><? echo $titel2;?> schneidet folgende<br>&Auml;mter des Kreises:</td>
+										<td bgcolor=<? echo $element_farbe ?> valign=top><? echo $label_auswahl;?> schneidet folgende<br>&Auml;mter des Kreises:</td>
 										<td bgcolor=<? echo $element_farbe ?>><b>
 											<?php 
 												for($x=0;$x<$i;$x++)
@@ -284,7 +282,7 @@ if ($lsg_id > 0)
 										</td>													
 									</tr>
 									<tr>
-										<td valign=top><? echo $titel2;?> schneidet folgende<br>Gemeinden des Kreises:</td>
+										<td valign=top><? echo $label_auswahl;?> schneidet folgende<br>Gemeinden des Kreises:</td>
 										<td><b>
 											<?php 
 												for($y=0;$y<$k;$y++)
