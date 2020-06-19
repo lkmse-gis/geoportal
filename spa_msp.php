74e759f86c0688edb95a7a7cec7403568fbccc1e
Andreas Thurm <andreas.thurm@lk-seenplatte.de>
2020-06-19 08:03:11 +0200

    SPA-Gebiete

--- spa_msp.php
+++ spa_msp.php
@@ -1,12 +1,17 @@
 <?php
 include ("includes/connect_geobasis.php");
-include ("includes/connect.php");
+include ("includes/connect_i_procedure_mse.php");
 include ("includes/portal_functions.php");
+require_once ("classes/karte.class.php");
+require_once ("classes/legende_geo.class.php");
+$layer_legende="Vogelschutzgebiete";
+$layer_legende_2="Kreisgrenze_msp";
+$layer="Vogelschutzgebiete";
+$label_auswahl="Vogelschutzgebiet";
 
 //globale Varibalen
 $titel="Vogelschutzgebiete";
-$titel2="Vogelschutzgebiet";
-$datei="spa_msp.php";
+$datei=$_SERVER["PHP_SELF"];
 $tabelle="sg_spa_fl";
 $schema="environment";
 $kuerzel="spafl";
@@ -14,17 +19,14 @@ $layerid="32070";
 
 $spafl_id=$_GET["$kuerzel"];
 
-$log=write_log($db_link,$layerid);
+$log=write_i_log($db_link,$layerid);
 
 if ($spafl_id < 1)
     { 
 		$query="SELECT COUNT(*) AS anzahl FROM $schema.$tabelle";	  
 		$result = $dbqueryp($connectp,$query);
 		$r = $fetcharrayp($result);
-		$count = $r[anzahl];
-	
-		$lon=368607;
-		$lat=5937811;
+		$count = $r["anzahl"];
 		?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml">
@@ -36,18 +38,10 @@ if ($spafl_id < 1)
 		<? include ("ajax.php"); ?>
 		<? include ("includes/zeit.php"); ?>
 		<? include ("includes/meta_popup.php"); ?>
-			<style type="text/css">
-				#map {
-				width: 710px;
-				height: 490px;
-				border: 1px solid black;
-				}
-			</style>
-		<script src=<? echo $openlayers_url; ?> type="text/javascript" language="Javascript"></script>
-			<link rel="stylesheet" href=<? echo $olstyles_url; ?> type="text/css" />
-		<script type="text/javascript" language="Javascript">
-			<? include ("includes/block_1.php"); ?>
-		</script>
+		<?
+             $geoportal_karte= new karte;
+             echo $geoportal_karte->zeigeKarteBox($box_mse_gesamt,'750','510','orka','1','0','0','0','0',$beschriftung_karte,$layer);			 
+            ?>
 		<script type="text/javascript" language="JavaScript1.2" src="um_menu.js"></script>
 		
 		</head>
@@ -76,7 +70,7 @@ if ($spafl_id < 1)
 								</tr>
 								<tr>
 									<td align="center"  height=30 colspan=2>
-										<? echo $titel2; ?> ausw&auml;hlen:
+										<? echo $label_auswahl; ?> ausw&auml;hlen:
 									</td>
 								</tr>
 								<tr>
@@ -96,63 +90,35 @@ if ($spafl_id < 1)
 										</form>
 									</td>									
 								</tr>
-                                <tr>
-					             <td valign=bottom align=center colspan=2>
-					    *) <a href="<? echo $metadatenpfad.$layerid;?>" target="_blank" onclick="return meta_popup(this.href)">Info zum Thema <? echo $titel; ?></a>
-						         </td>
-					            </tr>
-					            <tr><td align=center colspan=2>letzte Aktualisierung: <b><i><? echo get_aktualitaet($dbname,$layerid); ?></td>
-								</tr>
-								<tr>
-									<td valign=bottom align=center>
-										<a href="includes/kartehilfe.php" target="_blank" onclick="return hilfe_popup(this.href)">Wie nutze ich die webbasierte Karte?<br>(OpenLayers)</a>																
-									</td>
-								</tr>
-                                <tr>									
-										<td valign=bottom align=right>
-											<!-- Tabelle für Legende -->
-											<table border="1" rules="none" width=140 valign=bottom align=right>					
-												<tr>
-													<td colspan=4 align=center height=25><i>Kartenlegende:</i></td>
-												</tr>
-												<tr>
-													<td align=right><small><? echo $titel2;?>: </td>
-													<td align=right><img src="images/spafl.gif" width=30></td>													
-													<td align=right><small>Kreisgrenze: </td>
-													<td align=right><img src="images/gemeindegrenze_2.png" width=30></td>													
-												</tr>											
-											</table> <!-- Ende der Tabelle für die Legende -->
-										</td>					           
-								</tr>							
-								<tr>
-										<td colspan=2 height=35></td>                                       										
-										<td><small>
-											 <? echo $cr; ?>| &nbsp;<a href="includes/kartehilfe.php" target="_blank" onclick="return hilfe_popup(this.href)">Hilfe zur Kartennutzung</a>
-										</td>	
-										<td>   
-											<a href="<? echo $metadatenpfad.$layerid;?>" target="_blank" onclick="return hilfe_popup(this.href)"><img src="images/info_button.gif" title="Metadaten" border=0></a>
-										</td>
-										<td align=right>
-											<a href="<? echo $datei;?>"><img src="images/reload.png" title="Kartenausschnitt neu laden"></a>
-										</td>
-									</tr>
+								<!-- es folgt die Einbindung eines Snippets mit der Verknüpfung zu den Metadaten -->
+								
+                                <? include ("includes/meta_i_aktualitaet.php"); ?> 
+								
+								<!-- Zeile für die Legende -->
+								
+								<tr>									
+ 			                       <td valign=bottom align=left >
+							       <table class="table_legende" >
+								    <B>Kartenlegende :</B>
+								    <?php
+								     $legende_geo= new legende_geo;
+								     echo $legende_geo->zeigeLegende($layer_legende,$layer_legende_2,'','','');
+								     ?>
+							       </table> 
+						          </td>
+    		                   	</tr>
+
+							   <!-- Einbindung des Snippets für die Zeile unter der Karte -->
+							   
+					           <? include ("includes/block_1_1_uk.php"); ?>	
 							</table>
 						</div>
 					</div>
-					<div id="navigation">
-						<table border="0" align="left">
-							<tr>
-								<td>
-									<script type="text/javascript" language="JavaScript1.2" src="menu_msp_i.js"></script>
-								</td>
-							</tr>
-						</table>
-					</div>
-					<div id="extra">
-                    <? include ("includes/news.php") ?>					
-					</div>
-					<div id="footer">						
-					</div>
+			<?php 
+				echo div_navigation(); 
+				echo div_extra(); 
+				echo div_footer(); 
+			?>
 				</div>
 			</body>
 		</html>
@@ -181,41 +147,12 @@ if ($spafl_id > 0)
 		   $count=$k;
 		}
 	  
-	  $query="SELECT box(the_geom) as box, area(the_geom) as area, st_astext(st_centroid(st_transform(the_geom,2398))) as center, st_astext(st_centroid(st_transform(the_geom, 25833))) as utm, st_astext(st_centroid(st_transform(the_geom, 31468))) as rd83, st_astext(st_centroid(st_transform(the_geom, 4326))) as geo, st_astext(st_transform(the_geom, 2398)) as koordinaten, st_perimeter(the_geom) as umfang, gid, gebiet_nam, eu_nr, nr, gis_code FROM $schema.$tabelle WHERE gid='$spafl_id'";	  
+	  $query="SELECT box(st_transform(the_geom,25833)) as etrsbox, st_transform(the_geom,25833) as geom_25833, gid, gebiet_nam, eu_nr, nr, gis_code FROM $schema.$tabelle WHERE gid='$spafl_id'";	  
 	  $result = $dbqueryp($connectp,$query);
 	  $r = $fetcharrayp($result);
-	  $bildname = $r[ziel];
-	  $area=$r[area];	  
-	  $zentrum = $r[center];
-	  $zentrum2 = trim($zentrum,"POINT(");
-	  $zentrum3 = trim($zentrum2,")");
-	  $zentrum4 = explode(" ",$zentrum3);
-	  $rd83 = $r[rd83];
-	  $utm = $r[utm];
-	  $geo = $r[geo];
-	  $umfang = $r[umfang];
-	  $umfang2 = explode(".",$umfang);
-	  $umfang3 = $umfang2[0];
-	  $rcenter = $zentrum4[0];
-	  $rcenter1 = explode(".",$rcenter);
-	  $rcenter2 = $rcenter1[0];
-	  $hcenter = $zentrum4[1];
-	  $hcenter1 = explode(".",$hcenter);
-	  $hcenter2 = $hcenter1[0];
-	  $boxstring = $r[box];
-	  $klammern=array("(",")");
-	  $boxstring = str_replace($klammern,"",$boxstring);
-	  $koordinaten = explode(",",$boxstring);
-	  $rechts_range = $koordinaten[0]-$koordinaten[2];
-	  $rechts = $koordinaten[2]+($rechts_range/2);
-	  $hoch_range = $koordinaten[1]-$koordinaten[3];
-	  $hoch = $koordinaten[3]+($hoch_range/2);
-	  $range = $hoch_range;
-	  if ($rechts_range > $hoch_range) $range=$rechts_range;
-    
-
-        $lon=$rechts;
-		$lat=$hoch;
+	  $bildname = $r["ziel"];
+	  $etrsbox = $r["etrsbox"];
+	  $geom_25833=$r["geom_25833"];
 		?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml">
@@ -227,111 +164,10 @@ if ($spafl_id > 0)
 		<? include ("ajax.php"); ?>
 		<? include ("includes/zeit.php"); ?>
 		<? include ("includes/meta_popup.php"); ?>
-			<style type="text/css">
-			   #map {
-					width: 700px;
-					height: 320px;
-					border: 1px solid black;
-				}
-			</style>
-		<script src=<? echo $openlayers_url; ?> type="text/javascript" language="Javascript"></script>
-			<link rel="stylesheet" href=<? echo $olstyles_url; ?> type="text/css" />
-		<script type="text/javascript" language="Javascript">
-			var lon   = <?php echo $lon; ?>;
-			var lat   = <?php echo $lat; ?>;
-			var lonc  = <?php echo $rcenter; ?>;
-			var latc  = <?php echo $hcenter; ?>;
-             <?php
-              if ($hoch_range > 18000 OR $rechts_range > 18000) $zoom=9;
-			  else if ($hoch_range > 14000 AND $hoch_range < 17999 OR $rechts_range > 14000 AND $rechts_range < 17999) $zoom=10;
-			  else if ($hoch_range > 10000 AND $hoch_range < 13999 OR $rechts_range > 10000 AND $rechts_range < 13999) $zoom=11;
-			  else if ($hoch_range > 8000 AND $hoch_range < 9999 OR $rechts_range > 8000 AND $rechts_range < 9999) $zoom=16;
-			  else if ($hoch_range > 7000 AND $hoch_range < 8999 OR $rechts_range > 7000 AND $rechts_range < 7999) $zoom=17;
-			  else if ($hoch_range > 6000 AND $hoch_range < 6999 OR $rechts_range > 6000 AND $rechts_range < 6999) $zoom=20;
-			  else if ($hoch_range > 5000 AND $hoch_range < 5999 OR $rechts_range > 5000 AND $rechts_range < 5999) $zoom=21;
-			  else if ($hoch_range > 4000 AND $hoch_range < 4999 OR $rechts_range > 4000 AND $rechts_range < 4999) $zoom=22;
-			  else if ($hoch_range > 3000 AND $hoch_range < 3999 OR $rechts_range > 3000 AND $rechts_range < 3999) $zoom=23;
-			  else if ($hoch_range > 2000 AND $hoch_range < 2999 OR $rechts_range > 2000 AND $rechts_range < 2999) $zoom=24;
-              else $zoom=25;
-             ?>
-
-			var zoom  = <?php echo $zoom ?>;
-
-			var map, info;
-
-			function load() {
-				map = new OpenLayers.Map({
-					div: "map",
-					projection: "EPSG:2398",
-					scales: [750000,700000,650000,600000,550000,500000,450000,400000,350000,250000,200000,150000,100000,75000,70000,65000,60000,55000,50000,45000,40000,35000,30000,25000,20000,15000,10000,5000,2500,1000,500],
-					maxResolution: "auto",
-					maxExtent:  new OpenLayers.Bounds(4400000,5880000,4660000,6060000),
-					units: 'm'
-				});
-
-				var topomv = new OpenLayers.Layer.WMS.Untiled("Topografie",
-					<? echo $topo_url; ?>,
-					{'layers': 'mv_dtk', transparent: true, format: 'image/png'},
-					{isBaseLayer: true}
-				);
-
-				 var dop = new OpenLayers.Layer.WMS.Untiled("Luftbild",
-									<? echo $dop_url; ?>,
-									{'layers': 'mv_dop', transparent: true, format: 'image/png'},
-									{isBaseLayer: true}
-				);				
-				
-				var kreisgrenze = new OpenLayers.Layer.WMS.Untiled("Kreisgrenze",
-								 <? echo $map_msp_url; ?>,
-								 {layers: 'Kreisgrenze_msp', transparent: true, format: 'image/png'},
-								 {isBaseLayer: false}
-				);				
-
-				var <? echo $kuerzel;?> = new OpenLayers.Layer.WMS.Untiled("<? echo $titel;?>",
-								 <? echo $map_msp_url;?>,
-								 {layers: '<? echo $titel;?>', transparent: true, format: 'image/png'},
-								 {isBaseLayer: false}
-				);
-								
-				var markers = new OpenLayers.Layer.Markers( "Markers" );
-				
-				map.addLayers([topomv,dop,<? echo $kuerzel;?>,kreisgrenze]);
-
-				info = new OpenLayers.Control.WMSGetFeatureInfo({
-					layers: [<? echo $kuerzel;?>],
-					url: '<? echo $featureinfo_msp_url; ?>',
-					title: 'Identify features by clicking',
-					queryVisible: true,
-					eventListeners: {
-						getfeatureinfo: function(event) {
-							map.addPopup(new OpenLayers.Popup.FramedCloud(
-								"chicken",
-								map.getLonLatFromPixel(event.xy),
-								null,
-								event.text,
-								null,
-								true
-							));
-						}
-					}
-				});
-				map.addControl(info);
-				info.activate();
-				
-				map.addControl(new OpenLayers.Control.LayerSwitcher());
-				map.addControl(new OpenLayers.Control.Permalink());
-				map.addControl(new OpenLayers.Control.OverviewMap());
-				map.addControl(new OpenLayers.Control.MousePosition());
-				var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:2398"), map.getProjectionObject());
-				var lonLatc = new OpenLayers.LonLat(lonc, latc).transform(new OpenLayers.Projection("EPSG:2398"), map.getProjectionObject());
-				markers.addMarker(new OpenLayers.Marker(lonLatc));				
-				map.setCenter(lonLatc,zoom);
-			}
-		</script>
-		<style type="text/css">
-			td.rand {border: solid #000000 2px;}
-			td.rahmen {border: solid #000000 1px;}
-		</style> 
+		<?
+             $geoportal_karte= new karte;
+             echo $geoportal_karte->zeigeKarteBox($etrsbox,'700','520','orka','1','0','0','0','0',$beschriftung_karte,$layer);			 
+        ?> 
 		<script type="text/javascript" language="JavaScript1.2" src="um_menu.js"></script>
 		<script type="text/javascript">
 			function popup (url) {
@@ -357,7 +193,7 @@ if ($spafl_id > 0)
 								<table border=0>									
 									<tr>
 										<td height="40" align="center" valign=center width=250 colspan="2" bgcolor=<? echo $header_farbe; ?>>
-											<? echo $font_farbe ;?><? echo $r[gebiet_nam]; ?><? echo $font_farbe_End ;?>
+											<? echo $font_farbe ;?><? echo $r["gebiet_nam"]; ?><? echo $font_farbe_End ;?>
 										</td>
 										<td width=30 rowspan=7></td>
 										<td border=0 align=center rowspan="6" colspan=3>
@@ -366,11 +202,11 @@ if ($spafl_id > 0)
 									</tr>
 									<tr>
 										<td align="center" height="35" valign=center colspan="2" bgcolor=<? echo $element_farbe; ?>>
-											<b>EU-Nummer: <? echo $r[eu_nr]; ?></b><br>											
+											<b>EU-Nummer: <? echo $r["eu_nr"]; ?></b><br>											
 										</td>
 									</tr>
 									<tr>
-										<td align="center" height="25" colspan="2"><? echo $titel2;?>:</td>										
+										<td align="center" height="25" colspan="2"><? echo $label_auswahl;?>:</td>										
 									</tr>
 									<tr>
 										<td align="center" height="25" colspan="2">
@@ -381,7 +217,7 @@ if ($spafl_id > 0)
 														$result = $dbqueryp($connectp,$query);														
 														while($e = $fetcharrayp($result))
 														{
-														 echo "<option";if ($spafl_id == $e[gid]) echo " selected"; echo " value=\"$e[gid]\">$e[gebiet_nam]</option>\n";
+														 echo "<option";if ($spafl_id == $e["gid"]) echo " selected"; echo ' value="',$e["gid"],'">',$e["gebiet_nam"],'</option>\n';
 														}
 													?>
 												</select>
@@ -393,62 +229,49 @@ if ($spafl_id > 0)
 											<a href="<? echo $datei;?>"><? echo $font_farbe ;?>alle <? echo $titel;?><? echo $font_farbe_end ;?></a>
 										</td>										
 									</tr>									
-									<tr>										
-										<td valign=bottom align=right>
-											<!-- Tabelle für Legende -->
-											<table border="1" rules="none" width=140 valign=bottom align=right>					
-												<tr>
-													<td colspan=4 align=center height=25><i>Kartenlegende:</i></td>
-												</tr>
-												<tr>
-													<td align=right><small>Vogelschutzgebiet: </td>
-													<td align=right><img src="images/spafl.gif" width=30></td>
-													<td align=right><small>Kreisgrenze: </td>
-													<td align=right><img src="images/gemeindegrenze_2.png" width=30></td>
-												</tr>																							
-											</table> <!-- Ende der Tabelle für die Legende -->
-										</td>
-									</tr>	
-									<tr>
-										<td colspan=2></td>                                       										
-										<td><small>
-											 <? echo $cr; ?>| &nbsp;<a href="wissenswertes/kartehilfe.php" target="_blank" onclick="return hilfe_popup(this.href)">Hilfe zur Kartennutzung</a>
-										</td>	
-										<td>
-											<a href="<? echo $metadatenpfad.$layerid;?>" target="_blank" onclick="return hilfe_popup(this.href)"><img src="images/info_button.gif" title="Metadaten" border=0></a>
-										</td>
-										<td align=right>
-											<a href="<? echo $datei;?>?<? echo $kuerzel;?>=<? echo $nsg_id; ?>"><img src="images/reload.png" title="Kartenausschnitt neu laden"></a>
-										</td>
-									</tr>
-								</table>
+								<!-- Zeile für die Legende -->
+								
+								   <tr>									
+ 			                       <td valign=bottom align=left >
+							       <table class="table_legende" >
+								    <B>Kartenlegende :</B>
+								    <?php
+								     $legende_geo= new legende_geo;
+								     echo $legende_geo->zeigeLegende($layer_legende,$layer_legende_2,'','','');
+								     ?>
+							       </table> 
+						          </td>
+    		                   	</tr>
+							   <!-- Einbindung des Snippets für die Zeile unter der Karte -->
+							   
+					           <? include ("includes/block_1_1_uk.php"); ?>				</table>
 							</td>
 						</tr>
-					</table>
+					</table>	<!-- Ende äußere Tablle oberer Block -->
 					<table width="100%" border="0" cellpadding="0" align="center" cellspacing="0">
 						    <tr>
 							<td valign=top>											
 								<table border=0 valign=top>
 									<tr height="35">
-										<td colspan=3 width="620" bgcolor=<? echo $header_farbe ;?>>&nbsp;&nbsp;<? echo $font_farbe ;?><font size="+1"><? echo $r[gebiet_nam] ;?><? echo $font_farbe_end ;?></td>													
+										<td colspan=3 width="620" bgcolor=<? echo $header_farbe ;?>>&nbsp;&nbsp;<? echo $font_farbe ;?><font size="+1"><? echo $r["gebiet_nam"] ;?><? echo $font_farbe_end ;?></td>													
 									</tr>
 									<tr>
 										<td bgcolor=<? echo $element_farbe ?>>Nummer:</td>
-										<td bgcolor=<? echo $element_farbe ?>><b><? echo $r[nr] ;?></b></td>													
+										<td bgcolor=<? echo $element_farbe ?>><b><? echo $r["nr"] ;?></b></td>													
 									</tr>
 									<tr>
 										<td>EU-Nummer:</td>
-										<td><b><? echo $r[eu_nr] ;?></b></td>													
+										<td><b><? echo $r["eu_nr"] ;?></b></td>													
 									</tr>									
 									<tr height="30">
 										<td bgcolor=<? echo $element_farbe ?>>Anlage:</td>
 										<?
-											echo "<td bgcolor=$element_farbe><b><a href='pictures/spa_fl/$r[gis_code].pdf' target='_blank' onclick='return popup(this.href);'>$r[gis_code].pdf</a></b></td>";
+											echo '<td bgcolor=$element_farbe><b><a href="pictures/spa_fl/',$r["gis_code"],'.pdf" target=_blank onclick="return popup(this.href);">',$r["gis_code"],'.pdf</a></b></td>';
 										?>
 									</tr>
 									<tr height="30">
 										<td>GIS Code: </td>
-										<td><b><? echo $r[gis_code] ;?></b></td>
+										<td><b><? echo $r["gis_code"] ;?></b></td>
 									<tr>
 										<td bgcolor=<? echo $element_farbe ?> valign=top>Vogelschutzgebiet schneidet folgende<br>&Auml;mter des Kreises:</td>
 										<td bgcolor=<? echo $element_farbe ?>><b>
@@ -471,26 +294,17 @@ if ($spafl_id > 0)
 							</td>
 							<td width=30></td>
 							<td valign=top align=center width="350">
-								<? include ("includes/geo_flaeche.php") ?>
+								<? echo geo_flaeche($geom_25833,$connectp,$dbqueryp,$fetcharrayp) ?>
 							</td>
 						</tr>
 					</table>				
 				</div>
 			</div>
-			<div id="navigation">
-				<table border="0" align="left">
-					<tr>
-						<td>
-							<script type="text/javascript" language="JavaScript1.2" src="menu_msp_i.js"></script>
-						</td>
-					</tr>
-				</table>
-			</div>
-			<div id="extra">
-				<? include ("includes/news.php") ?>		
-			</div>
-			<div id="footer">			
-			</div>
+			<?php 
+				echo div_navigation(); 
+				echo div_extra(); 
+				echo div_footer(); 
+			?>
 		</div>
 		</body>
 		</html>
